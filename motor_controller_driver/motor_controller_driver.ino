6b56fb07da12cbb966776d23fadc628b168e7667
Thomas <thomasknechtel@outlook.com>
2022-11-04 11:09:47 -0400

    Fixed empty code, fix right motors not turning and DutyCycle bug

---
+++ motor_controller_driver/motor_controller_driver.ino
@@ -0,0 +1,17 @@
+#include "MotorControl.h"
+void setup() {
+  Serial.begin(9600);
+  setupMotorControl();
+}
+
+void loop() {
+  
+ 
+ //right();
+  // delay(1000);
+  left(17, 0.8);
+  // delay(1000);
+  // stop();
+  // delay(1000);
+
+}
