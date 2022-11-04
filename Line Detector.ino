6f8f03b5703f5520a7d93923a6df091aa47d2e82
KhusmeetAhluwalia <71512068+KhusmeetAhluwalia@users.noreply.github.com>
2022-11-04 09:54:51 -0400

    Add files via upload

---
+++ Line Detector.ino
@@ -0,0 +1,58 @@
+
+int Left = 8;
+int Middle = 9;
+int Right = 10;
+
+
+void setup() {
+pinMode(Left, INPUT);
+pinMode(Middle, INPUT);
+pinMode(Right, INPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+  String l;
+  String r;
+  String m;
+  int count = 0;
+  int Line_flag =0;
+int L_VAL= digitalRead(Left);  
+int M_VAL= digitalRead(Middle);
+int R_VAL= digitalRead(Right);
+  
+
+  if(L_VAL == 0){
+    l="W";
+  }else{
+    l="B";
+    count++;
+  }
+  
+    if(R_VAL == 0){
+    r="W";
+  }else{
+    r="B";
+    count++;
+  }
+
+    if(M_VAL == 0){
+    m="W";
+  }else{
+    m="B";
+    count++;
+  }
+
+  if(count>1){
+    Line_flag = 1;
+    Serial.println("On Black line");
+  }
+  
+  Serial.println("LEFT " +(String)l + "  MIDDLE "+(String)m +"  Right "+(String)r);
+
+
+  delay(2000);
+
+
+
+}
