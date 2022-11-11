de31c4ce20d97dfcf5c2e6015b309a8ab11d0f34
Thomas <thomasknechtel@outlook.com>
2022-11-11 11:59:27 -0500

    State machine for navigation

--- robot_controller/Headers/NavControl.hpp
+++ robot_controller/Headers/NavControl.hpp
@@ -2,8 +2,36 @@
 
 using namespace std;
 
-enum State{IDLE=0, NAVIGATE, AVOID_OBSTACLE, REROUTE};
-
-State updateState(State currentState){
+int x;
+enum State{IDLE=0, NAVIGATE, AVOID_OBSTACLE, REROUTE, AT_EDGE};
+enum Inputs{START=0, FORWARD_OBSTACLE, RIGHT_OBSTACLE, LEFT_OBSTACLE, OBSTACLE_AVOIDED, EDGE_DETECTED, REROUTE_COMPLETE};
+State updateState(State currentState, Inputs input){
 	switch(currentState){
-		case State
+		case State::IDLE:
+			if(input ==  Inputs::START){
+				return State::NAVIGATE;
+			}
+		case State::NAVIGATE:
+			if(input == Inputs::FORWARD_OBSTACLE){
+				return State::AVOID_OBSTACLE;
+			}
+			else if(input == Inputs::EDGE_DETECTED){
+				return State::AT_EDGE;
+			}
+		case State::AVOID_OBSTACLE:
+			if(input == Inputs::OBSTACLE_AVOIDED){
+				return State::REROUTE;
+			}
+		case State::REROUTE:
+			if(input == Inputs::REROUTE_COMPLETE){
+				return State::NAVIGATE;
+			}else if(input == Inputs::FORWARD_OBSTACLE){
+				return State::AVOID_OBSTACLE;
+			}
+				
+		default:
+			return -1;
+		
+	}
+	
+}
