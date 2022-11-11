8e72638e5c3c0c0ff4b962140e5d1051ba3d3d81
Thomas <thomasknechtel@outlook.com>
2022-11-11 08:47:08 -0500

    created directory for unit tests

---
+++ UnitTests/Headers/Gyro.h
@@ -0,0 +1,33 @@
+#include <Wire.h>
+#include <LSM6.h>
+LSM6 imu;
+
+void initializa_GYRO(){
+  Wire.begin();
+
+  if (!imu.init())
+    {
+      Serial.println("Failed to detect and initialize IMU!");
+    while (1);
+    }
+    imu.enableDefault();
+}
+
+float GYRO_X(){
+float Gx;
+imu.read();
+Gx = ((int16_t)imu.g.x) * 8.75/1000;
+return Gx;
+}
+void GYRO_Y(){
+  float Gy;
+  imu.read();
+Gy = ((int16_t)imu.g.y) * 8.75/1000;
+return Gy;
+}
+void GYRO_Z(){
+  float Gz;
+  imu.read();
+Gz = ((int16_t)imu.g.z) * 8.75/1000;
+return Gz;
+}
