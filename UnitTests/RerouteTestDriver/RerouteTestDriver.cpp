aa2dcab688e65a05be1f87dc31bab4b411340605
Thomas <thomasknechtel@outlook.com>
2022-11-22 15:57:38 -0500

    added RerouteTestDriver

---
+++ UnitTests/RerouteTestDriver/RerouteTestDriver.cpp
@@ -0,0 +1,8 @@
+#include "../../robot_controller/Headers/Reroute.hpp"
+#include <iostream>
+using namespace std;
+int main(){
+	cout << "Test Driver for Reroute" << endl;
+	Reroute reroute;
+	cout << "Starting State: " << reroute.currentState << endl;
+}
