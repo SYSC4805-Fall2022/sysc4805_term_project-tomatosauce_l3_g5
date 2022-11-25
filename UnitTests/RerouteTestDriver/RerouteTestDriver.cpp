0c0cf3af95445d736f6a19c10631f46e9c667732
Thomas <thomasknechtel@outlook.com>
2022-11-25 08:42:23 -0500

    allow for multiple inputs

--- UnitTests/RerouteTestDriver/RerouteTestDriver.cpp
+++ UnitTests/RerouteTestDriver/RerouteTestDriver.cpp
@@ -15,27 +15,27 @@ int main(){
 	cout << "AvoidObstacle State : " << reroute.avoidObstacle.stateToString() << endl;
 	
 	cout << endl << "Input: START_REROUTE";
-	reroute.newInput(Inputs::START_REROUTE);
+	reroute.newInput({Input::START_REROUTE});
 	cout << " Output: " << outputToString(reroute.output) << " State: " << reroute.stateToString() << endl;
 	cout << "AvoidObstacle State : " << reroute.avoidObstacle.stateToString() << endl;
 	
 	cout << endl << "Input: NONE";
-	reroute.newInput(Inputs::NONE);
+	reroute.newInput({Input::NONE});
 	cout << " Output: " << outputToString(reroute.output) << " State: " << reroute.stateToString() << endl;
 	cout << "AvoidObstacle State : " << reroute.avoidObstacle.stateToString() << endl;
 	
 	cout << endl << "Input: TURN_COMPLETE";
-	reroute.newInput(Inputs::TURN_COMPLETE);
+	reroute.newInput({Input::TURN_COMPLETE});
 	cout << " Output: " << outputToString(reroute.output) << " State: " << reroute.stateToString() << endl;
 	cout << "AvoidObstacle State : " << reroute.avoidObstacle.stateToString() << endl;
 	
 	cout << endl << "Input: NONE";
-	reroute.newInput(Inputs::NONE);
+	reroute.newInput({Input::NONE});
 	cout << " Output: " << outputToString(reroute.output) << " State: " << reroute.stateToString() << endl;
 	cout << "AvoidObstacle State : " << reroute.avoidObstacle.stateToString() << endl;
 	
 	cout << endl<< "Input: TURN_COMPLETE";
-	reroute.newInput(Inputs::TURN_COMPLETE);
+	reroute.newInput({Input::TURN_COMPLETE});
 	cout << " Output: " << outputToString(reroute.output) << " State: " << reroute.stateToString() << endl;
 	cout << "AvoidObstacle State : " << reroute.avoidObstacle.stateToString() << endl;
 	
@@ -45,32 +45,32 @@ int main(){
 	cout << "AvoidObstacle State : " << reroute.avoidObstacle.stateToString() << endl;
 	
 	cout << endl << "Input: START_REROUTE";
-	reroute.newInput(Inputs::START_REROUTE);
+	reroute.newInput({Input::START_REROUTE});
 	cout << " Output: " << outputToString(reroute.output) << " State: " << reroute.stateToString() << endl;
 	cout << "AvoidObstacle State : " << reroute.avoidObstacle.stateToString() << endl;
 	//No Obstacle Detected on either side so turn left
 	cout << endl << "Input: NONE";
-	reroute.newInput(Inputs::NONE);
+	reroute.newInput({Input::NONE});
 	cout << " Output: " << outputToString(reroute.output) << " State: " << reroute.stateToString() << endl;
 	cout << "AvoidObstacle State : " << reroute.avoidObstacle.stateToString() << endl;
 	
 	cout << endl << "Input: TURN_COMPLETE";
-	reroute.newInput(Inputs::TURN_COMPLETE);
+	reroute.newInput({Input::TURN_COMPLETE});
 	cout << " Output: " << outputToString(reroute.output) << " State: " << reroute.stateToString() << endl;
 	cout << "AvoidObstacle State : " << reroute.avoidObstacle.stateToString() << endl;
 	//Obstacle to right still so can't turn right
 	cout << endl << "Input: RIGHT_OBSTACLE";
-	reroute.newInput(Inputs::RIGHT_OBSTACLE);
+	reroute.newInput({Input::RIGHT_OBSTACLE});
 	cout << " Output: " << outputToString(reroute.output) << " State: " << reroute.stateToString() << endl;
 	cout << "AvoidObstacle State : " << reroute.avoidObstacle.stateToString() << endl;
 	//No Obstacle detected now so turn right
 	cout << endl << "Input: NONE";
-	reroute.newInput(Inputs::NONE);
+	reroute.newInput({Input::NONE});
 	cout << " Output: " << outputToString(reroute.output) << " State: " << reroute.stateToString() << endl;
 	cout << "AvoidObstacle State : " << reroute.avoidObstacle.stateToString() << endl;
 	
 	cout << endl<< "Input: TURN_COMPLETE";
-	reroute.newInput(Inputs::TURN_COMPLETE);
+	reroute.newInput({Input::TURN_COMPLETE});
 	cout << " Output: " << outputToString(reroute.output) << " State: " << reroute.stateToString() << endl;
 	cout << "AvoidObstacle State : " << reroute.avoidObstacle.stateToString() << endl;
 	//Test 3
@@ -79,57 +79,57 @@ int main(){
 	cout << "AvoidObstacle State : " << reroute.avoidObstacle.stateToString() << endl;
 	
 	cout << endl << "Input: START_REROUTE";
-	reroute.newInput(Inputs::START_REROUTE);
+	reroute.newInput({Input::START_REROUTE});
 	cout << " Output: " << outputToString(reroute.output) << " State: " << reroute.stateToString() << endl;
 	cout << "AvoidObstacle State : " << reroute.avoidObstacle.stateToString() << endl;
 	
 	cout << endl << "Input: NONE";
-	reroute.newInput(Inputs::NONE);
+	reroute.newInput({Input::NONE});
 	cout << " Output: " << outputToString(reroute.output) << " State: " << reroute.stateToString() << endl;
 	cout << "AvoidObstacle State : " << reroute.avoidObstacle.stateToString() << endl;
 	
 	cout << endl << "Input: TURN_COMPLETE";
-	reroute.newInput(Inputs::TURN_COMPLETE);
+	reroute.newInput({Input::TURN_COMPLETE});
 	cout << " Output: " << outputToString(reroute.output) << " State: " << reroute.stateToString() << endl;
 	cout << "AvoidObstacle State : " << reroute.avoidObstacle.stateToString() << endl;
 	
 	cout << endl << "Input: RIGHT_OBSTACLE";
-	reroute.newInput(Inputs::RIGHT_OBSTACLE);
+	reroute.newInput({Input::RIGHT_OBSTACLE});
 	cout << " Output: " << outputToString(reroute.output) << " State: " << reroute.stateToString() << endl;
 	cout << "AvoidObstacle State : " << reroute.avoidObstacle.stateToString() << endl;
 	//Obstacle detected in front so must avoid
 	cout << endl << "Input: FORWARD_OBSTACLE";
-	reroute.newInput(Inputs::FORWARD_OBSTACLE);
+	reroute.newInput({Input::FORWARD_OBSTACLE});
 	cout << " Output: " << outputToString(reroute.output) << " State: " << reroute.stateToString() << endl;
 	cout << "AvoidObstacle State : " << reroute.avoidObstacle.stateToString() << endl;
 	//turns left again
 	cout << endl << "Input: NONE";
-	reroute.newInput(Inputs::NONE);
+	reroute.newInput({Input::NONE});
 	cout << " Output: " << outputToString(reroute.output) << " State: " << reroute.stateToString() << endl;
 	cout << "AvoidObstacle State : " << reroute.avoidObstacle.stateToString() << endl;
 	//Moves forwards
 	cout << endl << "Input: TURN_COMPLETE";
-	reroute.newInput(Inputs::TURN_COMPLETE);
+	reroute.newInput({Input::TURN_COMPLETE});
 	cout << " Output: " << outputToString(reroute.output) << " State: " << reroute.stateToString() << endl;
 	cout << "AvoidObstacle State : " << reroute.avoidObstacle.stateToString() << endl;
 	// No obstacle detected on right so begin turn
 	cout << endl << "Input: NONE";
-	reroute.newInput(Inputs::NONE);
+	reroute.newInput({Input::NONE});
 	cout << " Output: " << outputToString(reroute.output) << " State: " << reroute.stateToString() << endl;
 	cout << "AvoidObstacle State : " << reroute.avoidObstacle.stateToString() << endl;
 	// Begin moving forward
 	cout << endl<< "Input: TURN_COMPLETE";
-	reroute.newInput(Inputs::TURN_COMPLETE);
+	reroute.newInput({Input::TURN_COMPLETE});
 	cout << " Output: " << outputToString(reroute.output) << " State: " << reroute.stateToString() << endl;
 	cout << "AvoidObstacle State : " << reroute.avoidObstacle.stateToString() << endl;
 	// No Obstacle detected on right so turn right
 	cout << endl << "Input: NONE";
-	reroute.newInput(Inputs::NONE);
+	reroute.newInput({Input::NONE});
 	cout << " Output: " << outputToString(reroute.output) << " State: " << reroute.stateToString() << endl;
 	cout << "AvoidObstacle State : " << reroute.avoidObstacle.stateToString() << endl;
 	
 	cout << endl<< "Input: TURN_COMPLETE";
-	reroute.newInput(Inputs::TURN_COMPLETE);
+	reroute.newInput({Input::TURN_COMPLETE});
 	cout << " Output: " << outputToString(reroute.output) << " State: " << reroute.stateToString() << endl;
 	cout << "AvoidObstacle State : " << reroute.avoidObstacle.stateToString() << endl;
 }	
